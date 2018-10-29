f58c94ff1e8627d36957375b396fc5db05c351d5
Kentaro Kaneki <k@Ken-2.local>
2018-10-29 12:53:47 -0400

    added definition titles

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::CardsController < ApplicationController
   def index
     cards = Card.where(deck_id: params[:deck_id])
+
     deck = Deck.find(params[:deck_id])
     deck_name = deck.name
-    render json: {deck_name: deck_name, cards: ActiveModel::Serializer::ArraySerializer.new(cards)}
+    term_title = deck.definition_title.title
+    duplicate_def_titles = deck.definition_titles
+    definition_titles = duplicate_def_titles.uniq{|title| title.id}
+
+    render json: {
+      deck_name: deck_name,
+      term_title: term_title,
+      definition_titles: ActiveModel::Serializer::ArraySerializer.new(definition_titles),
+      cards: ActiveModel::Serializer::ArraySerializer.new(cards)
+    }
   end
 end
