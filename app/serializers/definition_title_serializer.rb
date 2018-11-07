0d5ac76164d0abcbb40bf8d889bf9db990b532bf
Kentaro Kaneki <k@Ken-2.local>
2018-11-07 11:55:42 -0500

    Refactored Edit.js

--- app/serializers/definition_title_serializer.rb
+++ app/serializers/definition_title_serializer.rb
@@ -1,10 +1,6 @@
 class DefinitionTitleSerializer < ActiveModel::Serializer
   attributes :id, :title, :sequence
 
-  # def :title
-  #   object.title.order("title")
-  # end
-
   def sequence
     object.definitions.first.sequence
   end
