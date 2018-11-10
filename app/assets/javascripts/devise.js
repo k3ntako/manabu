66a942d6a4d239ff3bd81667976c66ca2ef6d1d1
Kentaro Kaneki <k@Ken-2.local>
2018-11-09 22:21:43 -0500

    Converted devise.js to ES5

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -1,38 +1,38 @@
-let logInForm = document.getElementById('devise-log-in');
-let logInInputs = document.getElementsByClassName("devise-log-in-input")
-let signUpForm = document.getElementById('devise-sign-up');
-let signUpInputs = document.getElementsByClassName("devise-sign-up-input")
+var logInForm = document.getElementById('devise-log-in');
+var logInInputs = document.getElementsByClassName("devise-log-in-input")
+var signUpForm = document.getElementById('devise-sign-up');
+var signUpInputs = document.getElementsByClassName("devise-sign-up-input")
 
-let clickLogIn = () => {
+function clickLogIn(){
   logInForm.style.display = "block";
   signUpForm.style.display = "none";
   logInButton.style.backgroundImage = "linear-gradient(#5eabd8,#3797CF)";
   signUpButton.style.backgroundImage = "linear-gradient(#808080,#AEAEAE)";
-  for(let i=0; i < logInInputs.length; i++){
+  for(var i=0; i < logInInputs.length; i++){
     logInInputs[i].disabled = false;
   }
-  for(let i=0; i < signUpInputs.length; i++){
+  for(var i=0; i < signUpInputs.length; i++){
     signUpInputs[i].disabled = true;
   }
 }
 
-let clickSignUp = () => {
+function clickSignUp(){
   logInForm.style.display = "none";
   signUpForm.style.display = "block";
   logInButton.style.backgroundImage = "linear-gradient(#808080,#AEAEAE)";
   signUpButton.style.backgroundImage = "linear-gradient(#5eabd8,#3797CF)";
-  for(let i=0; i < logInInputs.length; i++){
+  for(var i=0; i < logInInputs.length; i++){
     logInInputs[i].disabled = true;
   }
-  for(let i=0; i < signUpInputs.length; i++){
+  for(var i=0; i < signUpInputs.length; i++){
     signUpInputs[i].disabled = false;
   }
 }
 
-let logInButton = document.getElementById("devise-log-in-button")
+var logInButton = document.getElementById("devise-log-in-button")
 logInButton.addEventListener("click", clickLogIn);
 
-let signUpButton = document.getElementById("devise-sign-up-button")
+var signUpButton = document.getElementById("devise-sign-up-button")
 signUpButton.addEventListener("click", clickSignUp);
 
 clickLogIn()
