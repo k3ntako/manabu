02ee609da8702e2a2c146d525032ec4b3ada6ed6
Kentaro Kaneki <k@Ken-2.local>
2018-11-10 21:25:02 -0500

    Fixed Devise confirmation email

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -2,6 +2,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource) # In case you want to sign in the user
-    your_new_after_confirmation_path
+    new_session_path(resource_name)
   end
 end
