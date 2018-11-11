5a3c7ca5dbe66aee5413fedb4f078a366e1262f0
Kentaro Kaneki <k@Ken-2.local>
2018-11-10 19:35:42 -0500

    Add confirmation email during sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :masteries
   has_many :user_decks
