316dc208eb8fbfaee144fd806c636cf792827b07
Kentaro Kaneki <k@Ken-2.local>
2018-11-19 22:09:38 -0500

    Fixed confirmation email flash messages

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,22 +2,6 @@
 
 class Users::PasswordsController < Devise::PasswordsController
   skip_before_action :require_no_authentication, :only => [:edit, :update]
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
-
-  # POST /resource/password
-  # def create
-  #   super
-  # end
-
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
   def update
     super
     if resource.errors.empty?
@@ -25,15 +9,4 @@ class Users::PasswordsController < Devise::PasswordsController
       sign_in(resource_name, resource)
     end
   end
-
-  # protected
-
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
 end
