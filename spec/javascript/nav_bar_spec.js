5a36d434a2e80f39267bc4fc737a3dbc05695be9
Kentaro Kaneki <k@Ken-2.local>
2018-12-04 20:43:10 -0500

    Change password and profile photo.

--- spec/javascript/nav_bar_spec.js
+++ spec/javascript/nav_bar_spec.js
@@ -22,35 +22,34 @@ describe('Nav bar fetch user', () => {
         profile_image_url: ""
       }
     }
-  }
 
-  fetchMock.get('/api/v1/users', {
-    status: 200,
-    body: currentUser
-  });
-  wrapper = mount(
-    <NavBar location= {{pathname: "/"}}/>
-  )
-})
+    fetchMock.get('/api/v1/users', {
+      status: 200,
+      body: currentUser
+    });
+    wrapper = mount(
+      <NavBar location= {{pathname: "/"}}/>
+    )
+  })
 
-afterEach(fetchMock.restore)
+  afterEach(fetchMock.restore)
 
-describe('NavBar', () => {
-  it('renders a nav bar with a user logged in.', (done) => {
-    setTimeout(() => {
-      expect(wrapper.find('div.nav-dropdown-item').length).toEqual(0)
-      expect(wrapper.find('a').at(0).text()).toEqual("Manabu")
+  describe('NavBar', () => {
+    it('renders a nav bar with a user logged in.', (done) => {
+      setTimeout(() => {
+        expect(wrapper.find('div.nav-dropdown-item').length).toEqual(0)
+        expect(wrapper.find('a').at(0).text()).toEqual("Manabu")
 
-      wrapper.find('.fa-bars').simulate('click')
-      let dropdown = wrapper.find('div.nav-dropdown-item')
-      expect(dropdown.length).toEqual(5)
-      expect(dropdown.at(0).text()).toEqual("Flashcards")
-      expect(dropdown.at(1).text()).toEqual("Notes")
-      expect(dropdown.at(2).text()).toEqual("Reminders")
-      expect(dropdown.at(3).text()).toEqual("")
-      expect(dropdown.at(4).text()).toEqual("Sign Out")
-      done()
+        wrapper.find('.fa-bars').simulate('click')
+        let dropdown = wrapper.find('div.nav-dropdown-item')
+        expect(dropdown.length).toEqual(5)
+        expect(dropdown.at(0).text()).toEqual("Flashcards")
+        expect(dropdown.at(1).text()).toEqual("Notes")
+        expect(dropdown.at(2).text()).toEqual("Reminders")
+        expect(dropdown.at(3).text()).toEqual("")
+        expect(dropdown.at(4).text()).toEqual("Sign Out")
+        done()
+      })
     })
   })
-})
 })
